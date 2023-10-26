da26ac9973d12cd7cb78fcf6f031a1021784bd98
malek.kadri@esprit.tn <malekkadri>
2023-10-26 20:18:53 +0200

    Final

---
+++ database/migrations/2023_10_15_054356_create_orders_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number')->unique();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->float('sub_total');
+            $table->unsignedBigInteger('shipping_id')->nullable();
+            $table->float('coupon')->nullable();
+            $table->float('total_amount');
+            $table->integer('quantity');
+            $table->enum('payment_method',['cod','paypal'])->default('cod');
+            $table->enum('payment_status',['paid','unpaid'])->default('unpaid');
+            $table->enum('status',['new','process','delivered','cancel'])->default('new');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->foreign('shipping_id')->references('id')->on('shippings')->onDelete('SET NULL');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('country');
+            $table->string('post_code')->nullable();
+            $table->text('address1');
+            $table->text('address2')->nullable();
+            $table->timestamps();
+
+        });
+        // Schema::create('order_products', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->unsignedBigInteger('product_id');
+        //     $table->string('product_name');
+        //     $table->timestamps();
+        //     $table->foreign('product_id')
+        //     ->references('id')
+        //     ->on('orders')
+        //     ->onDelete('cascade');
+
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
